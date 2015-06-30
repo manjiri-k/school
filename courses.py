e8a5a2d3eb07d17c3bdc24330f9c4ccef0cb822d
manjiri <manjiri.kulkarni@jginfosystems.com>
2015-06-30 16:56:53 +0530

    binary field

--- courses.py
+++ courses.py
@@ -6,5 +6,7 @@ class courses_courses(osv.osv):
     _columns={'name':fields.char(string='Name of course',size=20),
               'duration':fields.integer(string="Duration"),
               'assignments':fields.integer(string="Assignments"),
-              'courses_school':fields.many2many('school.school','school_courses_rel','courses_id_rel','school_id_rel',string="List of Courses")
+              'attach':fields.binary("Bianry field"),
+              'courses_school':fields.many2many('school.school','school_courses_rel','courses_id_rel','school_id_rel',string="List of Courses"),
+              
               }
